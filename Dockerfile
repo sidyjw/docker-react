723fa5dfb527cb19745d110a852881e385c25616
Sidiney <sidineysilva1@gmail.com>
2019-06-22 00:41:50 -0300

    First commit

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM node:alpine as dev
+WORKDIR /app
+COPY package.json .
+RUN npm i
+COPY . .
+EXPOSE 3000
+ENTRYPOINT ["npm"]
+CMD ["start"]
+
+FROM node:alpine as builder
+WORKDIR /app
+COPY --from=dev /app .          
+RUN npm run build
+
+FROM nginx:alpine as prod
+COPY --from=builder /app/build /usr/share/nginx/html
--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>Olá bora mudar o mundo! Isso ai, ihuuu</p>
+				Por que çao pegaaaa É muito mais leveeeeeee
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Sei láaaa
+				</a>
+			</header>
+		</div>
+	);
 }
 
 export default App;
